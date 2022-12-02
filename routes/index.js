9e2d5cca5ba3cfb07374874315b4b61890cd5d76
chamsdinn <chams86@msn.com>
2022-12-02 15:38:07 +0100

    random books

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,23 @@
 const router = require("express").Router();
 const protectRoute = require("../middlewares/protectRoute");
+const Ebook = require("../models/Ebook.model");
 
-router.get("/", (req, res, next) => {
-  res.send("Server is running... 🏃‍♂️");
+router.get("/", async (req, res, next) => {
+  try {
+    const someBooks = await Ebook.find();
+    const randomBooks = [];
+
+    for (let index = 0; index < 6; index++) {
+      const randomBook = someBooks.splice(
+        Math.floor(Math.random() * someBooks.length),
+        1
+      )[0];
+      randomBooks.push(randomBook);
+    }
+    res.status(200).json(randomBooks);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.use(require("./search"));
