ba814a9a300443aadb2798f41b404710371058db
Guilhem Martin Saint Léon <guilhem.msl@gmail.com>
2022-12-05 17:55:08 +0100

    favorite books and delete favorites implemented

--- routes/oneBook.js
+++ routes/oneBook.js
@@ -1,14 +1,35 @@
 const router = require("express").Router();
+const isAuthenticated = require("../middlewares/jwt.middleware");
 const Ebook = require("../models/Ebook.model");
+const FavEbook = require("../models/FavEbook.model");
+const User = require("../models/User.model");
 
-router.get("/books/:id", async (req, res, next) => {
+router.get("/books/:id", isAuthenticated, async (req, res, next) => {
   try {
-    const myBook = await Ebook.findById(req.params.id);
+    const { id } = req.params;
+    const myBook = await Ebook.findById(id);
+    const currentUser = await User.findById(req.payload.id);
+    const favBook = await FavEbook.findOne({
+      ebook: myBook.id,
+      user: currentUser.id,
+    });
+
+    myBook._doc.isFaved = Boolean(favBook);
+
     console.log("myBook", myBook);
     res.status(200).json(myBook);
   } catch (error) {
     next(error);
   }
 });
+
+/* .post
+1º the ID of the book 
+2º the ID of the user
+3º change the user model and create an array of favorite books
+4º put the ID of the book inside that array
+
+
+*/
 
 module.exports = router;
