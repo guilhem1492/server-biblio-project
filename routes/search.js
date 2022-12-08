6b91acaabca94a3c5fd17a9cceea4326dd9c7c34
chamsdinn <chams86@msn.com>
2022-12-08 12:28:08 +0100

    fixes error message on auth route and fix search

--- routes/search.js
+++ routes/search.js
@@ -5,11 +5,11 @@ router.get("/search", async (req, res, next) => {
   try {
     const { title, author } = req.query;
 
+    if (!title && !author) return res.json([]); // to force users to provide input
+
     const searchQ = title
       ? { title: new RegExp(title, "gi") }
       : { "author.name": new RegExp(author, "gi") };
-
-    console.log(searchQ);
 
     const searchedBook = await Ebook.find(searchQ);
     res.status(200).json(searchedBook);
